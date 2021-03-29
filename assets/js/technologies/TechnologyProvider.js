a43cf707a1a45f074e3d7e4611cf8031579986f5
root <root@DESKTOP-2KLJLBF.localdomain>
2021-03-29 16:58:38 -0400

    Added vue to technology and event dashboard to projects.

--- assets/js/technologies/TechnologyProvider.js
+++ assets/js/technologies/TechnologyProvider.js
@@ -1,96 +1,98 @@
 const technologyCollection = [
-    {
-        name: "Git",
-        link: "images/technologies/git.png"
-    },
-    {
-        name: "React",
-        link: "images/technologies/react.png"
-    },
-    {
-        name: "HTML",
-        link: "images/technologies/html.png"
-    },
-    {
-        name: "CSS",
-        link: "images/technologies/css.png"
-    },
-    {
-        name: "Javascript",
-        link: "images/technologies/js.jpg"
-    },
-    {
-        name: "JSX",
-        link: "images/technologies/jsx.png"
-    },
-    {
-        name: "Reactstrap",
-        link: "images/technologies/reactstrap.png"
-    },
-    {
-        name: "Bootstrap",
-        link: "images/technologies/bootstrap.png"
-    },
-    {
-        name: "GitHub",
-        link: "images/technologies/github.png"
-    },
-    {
-        name: "VS Code",
-        link: "images/technologies/vscode.png"
-    },
-    {
-        name: "C#",
-        link: "images/technologies/csharp.png"
-    },
-    {
-        name: ".NET Framework",
-        link: "images/technologies/dotnetframework.png"
-    },
-    {
-        name: "Postman",
-        link: "images/technologies/postman.png"
-    },
-    {
-        name: "Cloudinary",
-        link: "images/technologies/cloudinary.png"
-    },
-    {
-        name: "Adobe Photoshop",
-        link: "images/technologies/photoshop.png"
-    },
-    {
-        name: "Adobe Illustrator",
-        link: "images/technologies/illustrator.jpg"
-    },
-    {
-        name: "JSON",
-        link: "images/technologies/JSON.png"
-    },
-    {
-        name: "SQL Server",
-        link: "images/technologies/SQL Server.png"
-    },
-    {
-        name: ".NET MVC",
-        link: "images/technologies/dotnetMVC.jpg"
-    },
-    {
-        name: "ASP.NET Core",
-        link: "images/technologies/ASP.NET Core.png"
-    },
-    {
-        name: "PostgreSQL",
-        link: "images/technologies/PostgreSQL.png"
-    },
-    {
-        name: "Semantic UI React",
-        link: "images/technologies/Semantic UI React.png"
-    }
-]
-
-
+  {
+    name: "Git",
+    link: "images/technologies/git.png",
+  },
+  {
+    name: "React",
+    link: "images/technologies/react.png",
+  },
+  {
+    name: "HTML",
+    link: "images/technologies/html.png",
+  },
+  {
+    name: "CSS",
+    link: "images/technologies/css.png",
+  },
+  {
+    name: "Javascript",
+    link: "images/technologies/js.jpg",
+  },
+  {
+    name: "Reactstrap",
+    link: "images/technologies/reactstrap.png",
+  },
+  {
+    name: "GitHub",
+    link: "images/technologies/github.png",
+  },
+  {
+    name: "VS Code",
+    link: "images/technologies/vscode.png",
+  },
+  {
+    name: "C#",
+    link: "images/technologies/csharp.png",
+  },
+  {
+    name: ".NET Framework",
+    link: "images/technologies/dotnetframework.png",
+  },
+  {
+    name: "Vue",
+    link: "images/technologies/vue.png",
+  },
+  {
+    name: "Postman",
+    link: "images/technologies/postman.png",
+  },
+  {
+    name: "Cloudinary",
+    link: "images/technologies/cloudinary.png",
+  },
+  {
+    name: "Adobe Photoshop",
+    link: "images/technologies/photoshop.png",
+  },
+  {
+    name: "Adobe Illustrator",
+    link: "images/technologies/illustrator.jpg",
+  },
+  {
+    name: "JSON",
+    link: "images/technologies/JSON.png",
+  },
+  {
+    name: "SQL Server",
+    link: "images/technologies/SQL Server.png",
+  },
+  {
+    name: ".NET MVC",
+    link: "images/technologies/dotnetMVC.jpg",
+  },
+  {
+    name: "ASP.NET Core",
+    link: "images/technologies/ASP.NET Core.png",
+  },
+  {
+    name: "PostgreSQL",
+    link: "images/technologies/PostgreSQL.png",
+  },
+  {
+    name: "Semantic UI React",
+    link: "images/technologies/Semantic UI React.png",
+  },
+  {
+    name: "JSX",
+    link: "images/technologies/jsx.png",
+  },
+  {
+    name: "Bootstrap",
+    link: "images/technologies/bootstrap.png",
+  },
+];
 
 export const useTechnologies = () => {
-    return technologyCollection.slice()
-}
+  return technologyCollection.slice();
+};
